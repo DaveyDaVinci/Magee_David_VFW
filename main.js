e90cfa16c771725514d681ea6d6f6ac429ae4080
DaveyDaVinci <davidmagee89@yahoo.com>
2012-05-06 23:24:35 -0400

    Updated a clear data link in html, updated js to include main function.

--- main.js
+++ main.js
@@ -3,4 +3,39 @@ David Magee
 VFW 0512
 */
 
-window.addEventListener("DOMContentLoaded", function(){});
+window.addEventListener("DOMContentLoaded", function(){
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	};
+	
+	var homePlanets = [ "Home Planet". "Mars", "Earth", "Pluto", "Vegas", "Turos", 
+		"Heisinkr", "Velarius"];
+		
+	var displayLink $('displaydata');
+	displayLink.addEventListener()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+/* 
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	};
+*/
