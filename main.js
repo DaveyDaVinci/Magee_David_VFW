3ce2e15ec2555401532bbbe83da9a7d165a9278f
DaveyDaVinci <davidmagee89@yahoo.com>
2012-05-07 00:57:27 -0400

    Updated with semi-working dropdown for Planets.

--- main.js
+++ main.js
@@ -9,16 +9,43 @@ window.addEventListener("DOMContentLoaded", function(){
 		return theElement;
 	};
 	
-	var homePlanets = [ "Home Planet", "Mars", "Earth", "Pluto", "Vegas", "Turos", 
-		"Heisinkr", "Velarius"];
-		
+	//Adds options for planets
+	function listPlanets (){
+		var formTag = document.getElementsByTagName("form"), //This is an array
+			selectLi = $('derp'),
+			makeHomePlanet = document.createElement('select');
+			makeHomePlanet.setAttribute("id", "homeplanets");
+		for(i=0, j=homePlanets.length; i<j; i++){
+			var createOption = document.createElement('option');
+			var optText = homePlanets[i]; 
+			createOption.setAttribute("value", optText);
+			createOption.innerHTML = optText;
+			makeHomePlanet.appendChild(createOption);
+		}
+		selectLi.appendChild(makeHomePlanet);
+	};
+	
+	//Adds options for skills
+	
+	
+	//Planets Array
+	var homePlanets = ["Allied Planets", "Mars", "Earth", "Pluto", "Vegas", 
+	"Relkin Group", "Turos", "Heisinkr", "Velarius"];
+	
+	//calls to functions
+	listPlanets();
+	
+	//Button Presses	
 	var displayLink = $('displaydata');
 	displayLink.addEventListener("click", getData);
 	var clearLink = $('cleardata');
 	clearLink.addEventListener("click", clearData);
-	var saveLink = $('savedata');
+	var saveLink =  $('savedata');
 	saveLink.addEventListener("click", saveData);
 
+
+	//calls to functions
+	listPlanets();
 
 
 
